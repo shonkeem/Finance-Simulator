6db3071707725b9ab2c9e73604070c7a169384a2
Sean Kim <seankim.hahjean@gmail.com>
2026-01-24 19:45:10 -0500

    basic frontend form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function submitData(submitEvent) {
+  submitEvent.preventDefault()
+  const data = new FormData(submitEvent.target)
+  const values = Object.fromEntries(data.entries())
+  console.log(values);
+  alert(`You searched for '${values.name}, ${values.age}, ${values.income}'`);
+}
 
+function MyForm() {
+  return (
+    <form onSubmit={submitData}>
+      <label>Name:</label><br />
+      <input name='name' type='text'/><br />
+      <label>Age:</label><br />
+      <input name='age' type='number'/><br />
+      <label>Income:</label><br />
+      <input name='income' type='number'/><br />
+      <label>Expenses:</label><br />
+      <input name='expenses' type='number'/><br />
+
+      <button type='submit'>Simulate</button>
+    </form>
+  )
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <MyForm />
+   </>
   )
 }
 
